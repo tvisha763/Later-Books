e2d17575f6601ed68229396fb2398982ae3e147a
Tvisha Choubey <76886688+tvisha763@users.noreply.github.com>
2022-04-26 17:44:44 -0700

    Update urls.py

--- later/urls.py
+++ later/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name='login'),
+    path('', views.login, name='login'),
     path('signup', views.signup, name='signup'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('logout', views.logout, name='logout'),
